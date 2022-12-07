a3fd80084fcdbf6935509f31b48179de89e72a3d
Yonatan Mengesha <yonatan.mengesha2017@gmail.com>
2022-12-07 15:52:26 -0500

    created the view. closes #5

--- src/main/java/guru/springframwork/spring5jokesappv2/controller/JockController.java
+++ src/main/java/guru/springframwork/spring5jokesappv2/controller/JockController.java
@@ -1,6 +1,7 @@
 package guru.springframwork.spring5jokesappv2.controller;
 
 import guru.springframwork.spring5jokesappv2.services.JockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JockController {
 
     private final JockService jockService;
-
+    @Autowired
     public JockController(JockService jockService) {
         this.jockService = jockService;
     }
